71b9c0fd9d4375a490cae70fb17503bc4130ce99
Afandi-FA <67248166+Afandi-FA@users.noreply.github.com>
2024-03-11 18:10:36 +0700

    Add files via upload

--- finalProject.py
+++ finalProject.py
@@ -9,6 +9,7 @@ st.set_page_config(
 )
 
 
+
 st.sidebar.header('Open Mindset')
 video_file = open('data.mp4', 'rb')
 video_bytes = video_file.read()
@@ -16,11 +17,33 @@ st.sidebar.video(video_bytes)
 st.sidebar.markdown("[Gita Irawan Wirjawan, B.B.A., M.B.A., M.P.A.](https://www.youtube.com/shorts/3B7gpUSPIDU)")
 
 
-
 st.title('Populasi No 4 di Dunia, Jumlah Pelajar Internasional _Tidak Masuk_ Top 20.')
 st.caption("Oleh: Muhammad Dzikry Afandi")
-st.header("Gambaran mahasiswa internasional, Indonesia.")
+# st.header("Gambaran mahasiswa internasional, Indonesia.")
 
+
+col1, col2 =  st.columns([2,3])
+with col1:
+  st.subheader("Latar Belakang")
+  st.markdown("Dengan semakin terbukanya kesempatan untuk menempuh pendidikan di luar negeri, khususnya di perguruan tinggi terbaik dunia, para pelajar di negara berkembang tentunya memiliki peluang yang besar untuk merasakan kualitas pendidikan tinggi yang setara.")
+  # st.markdown("Dalam era globalisasi pendidikan, pelajar dari negara berkembang semakin melihat peluang yang besar dalam menempuh pendidikan di perguruan tinggi terbaik dunia. Fenomena ini didorong oleh persepsi akan kualitas pendidikan tinggi yang tinggi, standar akademik yang unggul, dan fasilitas pendidikan yang modern. Gelar dari universitas terkemuka dianggap sebagai investasi dalam karier masa depan, membuka pintu untuk peluang kerja yang lebih baik. Faktor-faktor ini, bersama dengan kemudahan akses dan mobilitas internasional, menjadikan opsi pendidikan di luar negeri sebagai pilihan menarik bagi para pelajar di negara berkembang yang mencari pengalaman pendidikan tinggi yang berkualitas.")
+
+with col2:
+  st.subheader("Projek Objektif")
+  st.markdown("Projek ini bertujuan untuk menganalisis persebaran mahasiswa internasional di universitas-universitas terkemuka di seluruh dunia, termasuk jumlah mahasiswa Indonesia yang berkuliah di luar negeri setiap tahun. Dengan analisis ini, kami ingin memberikan pemahaman yang lebih baik tentang negara dan universitas yang menjadi tujuan utama mahasiswa internasional, tren perkembangan mahasiswa di universitas-universitas terbaik, serta jumlah dan tren pertumbuhan mahasiswa Indonesia di luar negeri. Kami berharap hasil analisis ini dapat memberikan wawasan yang berharga bagi pemerintah dalam merancang kebijakan untuk mendorong pertumbuhan mahasiswa internasional asal Indonesia, serta langkah-langkah untuk meningkatkan partisipasi dan akses pendidikan tinggi internasional bagi masyarakat Indonesia.")
+
+st.subheader("Key Questions")
+st.markdown('''
+            - Apa saja universitas terkemuka di seluruh dunia yang menjadi tujuan utama mahasiswa internasional, dan bagaimana persebaran mereka menurut wilayah geografis?
+            - Bagaimana tren jumlah mahasiswa internasional di universitas-universitas terbaik selama beberapa tahun terakhir?
+            - Berapa jumlah mahasiswa Indonesia yang berkuliah di luar negeri setiap tahunnya, bagaimana perbandingannya dengan negara negara tetangga?
+            - Apa saja negara tujuan utama bagi mahasiswa Indonesia yang berkuliah di luar negeri, dan bagaimana tren persebaran mereka dalam beberapa tahun terakhir?
+''')
+
+st.write('')
+st.write('')
+st.write('')
+st.write('')
 st.subheader("Persebaran Peringkat Universitas Dunia")
 
 
@@ -58,46 +81,52 @@ st.subheader("Persaingan Peringkat 10 Besar Dunia")
 
 
 # ------------------------Line Chart------------------------
+# Membaca data
 rank_univ = pd.read_csv('./data/rankUniv.csv')
 
 data = rank_univ[['NO','Tahun','Rank','Nama','Region/Country']]
 
-# Filtering to only take the top 10 rankings
-top_10_rank = data[data['Rank'] <= 10]
+# Mengambil nilai unik peringkat
+min_rank = int(data['Rank'].min())
+max_rank = int(data['Rank'].max())
+
+# Widget slider untuk memilih peringkat
+selected_rank = st.slider("Pilih Peringkat", min_value=min_rank, max_value=max_rank, value=(min_rank, min(min_rank+4, max_rank)))
+
+# Filtering data untuk hanya mengambil peringkat terpilih
+filtered_data = data[data['Rank'].between(selected_rank[0], selected_rank[1])]
 
 # Pivot the data to have years as columns
-pivot_data = top_10_rank.pivot_table(index='Nama', columns='Tahun', values='Rank', aggfunc='first')
+pivot_data = filtered_data.pivot_table(index='Nama', columns='Tahun', values='Rank', aggfunc='first')
 
 # Plotting line chart
 fig = px.line(pivot_data.T,  # Transpose pivot_data so that years become x-axis
-  labels={'value': 'Rangking', 'index': 'Year', 'variable': 'University'}
-  )
+              labels={'value': 'Ranking', 'index': 'Year', 'variable': 'University'}
+             )
+
 # Menambahkan mode 'lines+markers' untuk menampilkan titik pada setiap data
 fig.update_traces(mode='lines+markers')
+
 
 # Mengatur ketebalan garis
 for trace in fig.data:
     trace.line.width = 2  # Menetapkan ketebalan garis menjadi 2 px
 
+# Menampilkan plot
 st.plotly_chart(fig, use_container_width=True, width=None, height=600)
 
 
+st.markdown('''Berbanding lurus dengan dominasi Amerika serikat, 3 dari top 5 berasal dari negara Amerika, 7 dari top 10 juga diisi oleh negara tersebut. Namun University of Oxford, United states masihlah **konsisten dipuncak** selama 7 tahun belakangan ini.[2](https://www.timeshighereducation.com/world-university-rankings/2024/world-ranking)
 
-st.markdown('''Berbanding lurus dengan dominasi Amerika serikat, 7 dari top 10 diisi oleh negara tersebut. Namun University of Oxford, United states masihlah **konsisten dipuncak**.[2](https://www.timeshighereducation.com/world-university-rankings/2024/world-ranking)
-
-ini menarik, dua belas universitas diatas adalah beberapa tempat terbaik untuk belajar, role model universitas dunia.''')
+Ini menarik, berikut adalah persebaran mahasiswa internasional dari beberapa univeristas yang masuk kedalam 10 universitas terbaik''')
 
 
 with st.expander("University of California, Berkeley"):
   df_california = pd.read_csv('./data/california.csv')
-
-  # california
   data = df_california[['Year','Student','Country']]
 
   # Grouping data by Year and Country and summing up Student
   grouped_data = data.groupby(['Year', 'Country']).sum().reset_index()
-
-  
   last_year_data = grouped_data[grouped_data['Year'] == grouped_data['Year'].max()]
 
   # Menentukan top countries berdasarkan jumlah mahasiswa pada tahun terakhir
@@ -344,42 +373,45 @@ with st.expander("Stanford University"):
 
 
 
-st.markdown("dari persebaran di 7 universitas terbaik, pelajar internasional semuanya dikuasai oleh **negeri China**. nama nama yang sering muncul adalah India, Korea Selatan, Kanada, Australia, Prancis, Germany, Singapore, Jepang, Brazil, United Kingdom, dan Taiwan")
+st.markdown("Dari ketujuh universitas terbaik dunia tersebut, jumlah pelajar internasional semuanya dikuasai oleh **Negeri China**. nama nama yang sering muncul adalah India, Korea Selatan, Kanada, Australia, Prancis, Germany, Singapore, Jepang, Brazil, United Kingdom, dan Taiwan")
 st.subheader("Bagaimana Indonesia?")
 st.markdown("Lalu, berapa mahasiswa indonesia yang belajar di universitas-universitas tersebut?")
 
 
 
-df_all = pd.read_csv('./data/mahasiswaIndo.csv')
-# Membuat scatter plot
-fig = px.scatter(df_all, x='YEAR', y='Student', color='Universitas', hover_data=['Country'])
 
-# Mengubah mode pada mode bar menjadi 'lines' dan menambahkan line_shape='linear'
-fig.update_traces(mode='lines+markers', line=dict(shape='linear'), selector=dict(type='scatter'), showlegend=True)
+# Membaca data
+df_all = pd.read_csv('./data/mahasiswaIndo.csv')
+
+# Membuat line chart
+fig = px.line(df_all, x='YEAR', y='Student', color='Universitas', hover_data=['Country'])
 
 # Menambahkan garis rata-rata untuk setiap tahun
 avg_student = df_all.groupby('YEAR')['Student'].mean().reset_index()
 fig.add_trace(go.Scatter(x=avg_student['YEAR'], y=avg_student['Student'], mode='lines', name='Average'))
 
-fig.layout.legend.x = -0.3
+# Menambahkan dot di setiap data
+for data in fig.data:
+    data.update(mode='lines+markers')
 
 # Menampilkan plot
 st.plotly_chart(fig, use_container_width=True, height=600)
 
 
-st.markdown("diagram diatas menggambarkan bahwa **sedikit sekali** warga indonesia yang menempuh pendidikan di universitas universitas tersebut, hanya hitungan puluhan. **Rata-rata ditahun 2021 hanya 83 orang dan 60 di 2020**, paling banyak di angka 187 yang berlokasi di California University. namun disisi lain, tren pertumbuhannya **cenderung meningkat**, terutama di Stanford University dan Oxford University.")
+st.markdown("Terlihat bahwa **sedikit sekali** warga indonesia yang menempuh pendidikan di universitas univeritas terbaik dunia tersebut, hanya hitungan puluhan. **Rata-rata ditahun 2021 hanya 83 orang dan 60 di 2020**, paling banyak di tahun 2023 dengan jumlah 187 orang yang berlokasi di California University. Namun disisi lain, tren pertumbuhannya **cenderung meningkat**, terutama di Stanford University dan Oxford University.")
 
 st.subheader("Berapa Mahasiswa Indonesia?")
-st.markdown("Untuk melihat bagaimana posisi indonesia dibandingkan negara negada lainnya, berikut adalah persebaran mahasiswa internasional tiap tiap negara.")
+st.markdown("Untuk melihat bagaimana posisi indonesia dibandingkan negara negara lainnya, berikut adalah persebaran mahasiswa internasional tiap tiap negara.")
 
 
+# Membaca data
 df_studentworldseris = pd.read_csv('./data/internationalStudent.csv')
 
 # Filter 5 negara teratas
 top_countries = df_studentworldseris.groupby('Country')['Value'].sum().nlargest(5).index
 
-# Dropdown untuk memilih tahun
-selected_year = st.selectbox("Pilih Tahun", df_studentworldseris['Year'].unique())
+# Dropdown untuk memilih tahun dengan nilai default ke tahun terakhir
+selected_year = st.selectbox("Pilih Tahun", df_studentworldseris['Year'].unique(), index=len(df_studentworldseris['Year'].unique())-1)
 
 # Filter data berdasarkan tahun yang dipilih
 df_selected_year = df_studentworldseris[df_studentworldseris['Year'] == selected_year]
@@ -413,11 +445,10 @@ for index, row in df_combined_sorted.iterrows():
                          name=row['Country'], 
                          orientation='h',
                          text=f'Rank: {row["row_num"]}, Value: {row["Value"]}',
-                        #  textposition='outside',
                          marker=dict(color=color)))  # Beri warna pada bar
 
 # Menambahkan layout
-fig.update_layout(title=f'Rangking mahasiswa internasional berdasarkan negara pada tahun{selected_year}',
+fig.update_layout(title=f'Rangking mahasiswa Indonesia pada tahun {selected_year}',
                   xaxis_title='Jumlah Mahasiswa',
                   yaxis_title='Negara',
                   showlegend=False
@@ -427,10 +458,10 @@ fig.update_layout(title=f'Rangking mahasiswa internasional berdasarkan negara pa
 st.plotly_chart(fig, use_container_width=True)
 
 
-st.markdown("Berdasarkan data dari UIS Unesco [3](http://data.uis.unesco.org/). indonesia per 2021 berada **diperingkat 24**, turun 6 peringkat dari tahun sebelumnya, dari 58.491 menjadi 45.896. sangat jauh apabila dibandingkan dengan India apalagi China. Hal ini tentunya berimbas kepada kemampuan muda mudi indonesia.")
+st.markdown("Berdasarkan data dari UIS Unesco [3](http://data.uis.unesco.org/). indonesia per 2021 berada **diperingkat 24**, turun 6 peringkat dari tahun sebelumnya, dari 58.491 menjadi 45.896. sangat jauh apabila dibandingkan dengan India apalagi China. Hal ini tentunya perlu dipertanyakan, apakah masyarakat indonesia tidak mampu bersaing di kancah internasional?")
 
 st.subheader("Kemana Mahasiswa Indonesia?")
-st.markdown("Menarik untuk ditelusuri kemana saja mahasiswa indonesia menempuh pendidikan, ini bisa dijadikan proyeksi persebaran terbanyak.")
+st.markdown("Setelah melihat peringkat dan persebaran mahasiswa indonesia, menarik untuk ditelusuri kemana saja mahasiswa indonesia yang berkuliah di luar negeri ini menempuh pendidikan, ini bisa dijadikan proyeksi persebaran terbanyak tujuan mahasiswa internasional asal indonesia.")
 
 # Memuat data dari file CSV
 df_studentworldseris = pd.read_csv('./data/indonesianStudent.csv')
@@ -441,15 +472,15 @@ fig = px.treemap(df_studentworldseris, path=['Time', 'Country'], values='Value')
 # Menampilkan plot
 st.plotly_chart(fig, use_container_width=True)
 
-st.markdown("**Australia** yang menjadi negara tujuan pelajar indonesia, 1 dari 4 mahasiswa internasional kita ada di sana, tempat kedua ada Malaysia diangka 9000-an. lalu baru Amerika Serikat.")
+st.markdown("Terlihat bahwa **Australia** yang menjadi negara tujuan pelajar indonesia, 1 dari 4 mahasiswa internasional kita ada di sana, tempat kedua ada Malaysia diangka 9000-an. lalu baru Amerika Serikat.")
 
 st.subheader("Insight")
 st.markdown("""
-            - jumlah mahasiswa internasional indonesia masih sedikit.
-            - universitas terbaik terpusat di negara amerika.
-            - mahasiswa indonesia yang berada di universitas top 10 rata rata hanya 83 mahasiswa saja.
-            - destinasi utama pelajar internasional asal indonesia adalah Australia, Malaysia dan Amerika Serikat.
-            - adanya trend pertumbuhan di stanford university dan oxford univesity.
+            - Jumlah mahasiswa indonesia hanya % dari populasi, sebagai negara dengan populasi per 2021 sebesar 275 juta, mahaiswa asal indonesia di luar negeri hanya berjumlah 45 ribu.
+            - universitas terbaik terpusat di negara amerika. 53 universitas di negara amerika masuk kedalam 100 universitas terbaik, 7 universitas di amerika juga masuk kedalam 10 universitas terbaik.
+            - mahasiswa indonesia yang berada di universitas top 10 per tahun 2023 rata rata hanya 83 mahasiswa, naik dari tahun sebelumnya yang hanya 59 mahasiswa. 
+            - destinasi utama pelajar internasional asal indonesia adalah Australia, Malaysia dan Amerika Serikat. 1 dari 4 pelajar internasional asal indonesia berada di australia
+            - adanya trend pertumbuhan di california university dan oxford univesity. masing masing di tahun 2005 ada diangka 14 mahasiswa dan 10 mahasiswa, lalu di tahun 2023 masing masing menjadi 187 mahasiswa dan 100 mahasiswa.
             """)
 
 st.subheader("Apa selanjutnya?")
@@ -461,3 +492,4 @@ st.markdown("""
 """)
 
 st.markdown('''Contact Me: afandiahmed24@gmail.com LinkedIn: [Muhammad Dzikry Afandi](https://www.linkedin.com/in/muhammaddzikryafandi/)''')
+
